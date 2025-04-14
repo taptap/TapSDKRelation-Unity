298f29bb814f131f66827023785a5ba564a9d1b7
lc1006 <1176168734@qq.com>
2025-04-14 12:51:31 +0800

    feat:update upm

---
+++ Mobile/Editor/TapRelationMobileProcessBuild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor.Build.Reporting;
+using TapSDK.Core.Editor;
+
+namespace TapTap.Relation.Mobile.Editor {
+    public class TapRelationMobileProcessBuild : SDKLinkProcessBuild {
+        public override int callbackOrder => 0;
+
+        public override string LinkPath => "TapSDK/Relation/link.xml";
+
+        public override LinkedAssembly[] LinkedAssemblies => new LinkedAssembly[] {
+                    new LinkedAssembly { Fullname = "TapTap.Relation.Runtime" },
+                    new LinkedAssembly { Fullname = "TapTap.Relation.Mobile.Runtime" }
+                };
+
+        public override Func<BuildReport, bool> IsTargetPlatform => (report) => {
+            return BuildTargetUtils.IsSupportMobile(report.summary.platform);
+        };
+    }
+}
---
+++ Runtime/Internal/Init/RelationInitTask.cs
@@ -0,0 +1,20 @@
+﻿using TapSDK.Core;
+using TapSDK.Core.Internal.Init;
+using UnityEngine;
+
+namespace TapSDK.Relation.Internal.Init {
+    public class RelationInitTask : IInitTask {
+        public int Order => 104;
+
+        public void Init(TapTapSdkOptions coreOption, TapTapSdkBaseOptions[] otherOptions)
+        {
+            Debug.LogWarningFormat("[TapSDK-init]Relation task init: " + "clientId=" + coreOption.clientId + ", region=" + coreOption.region + ", screenOrientation=" + coreOption.screenOrientation);
+            TapTapRelationManager.Instance.Init(coreOption.clientId, coreOption.region, coreOption.screenOrientation);
+        }
+
+        public void Init(TapTapSdkOptions coreOption)
+        {
+            TapTapRelationManager.Instance.Init(coreOption.clientId, coreOption.region, coreOption.screenOrientation);
+        }
+    }
+}
